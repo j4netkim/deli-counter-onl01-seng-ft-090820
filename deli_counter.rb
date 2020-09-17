ab0112eba3dc06dd82384652eb42e409ff2f3969
J K <kj688895@gmail.com>
2020-09-17 17:16:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,3 +13,16 @@ def now_serving(katz_deli)
     puts "Currently serving #{katz_deli.shift}."
   end
 end
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    say = "The line is currently:"
+
+    katz_deli.each_with_index do |value, index|
+      say += "#{index.to_i+1}. #{value}"
+    end
+    puts "#{say}"
+  end
+end
