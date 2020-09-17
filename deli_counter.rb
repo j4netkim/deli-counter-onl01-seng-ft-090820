eedddd919276b1c5bd352376f2187ed8a0c57192
J K <kj688895@gmail.com>
2020-09-17 16:32:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def line(array)
+  katz_deli = []
+  array.each do |name|
+    puts "The line is currently empty."
+
+
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving()
