d9fc600c70b17b572dbc9f3602be59bc241f0898
J K <kj688895@gmail.com>
2020-09-17 17:20:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,6 +6,7 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
+
 def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
@@ -14,15 +15,15 @@ def now_serving(katz_deli)
   end
 end
 
+
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    say = "The line is currently:"
-
+    say_this = "The line is currently:"
     katz_deli.each_with_index do |value, index|
-      say += "#{index.to_i+1}. #{value}"
+      say_this += " #{index.to_i + 1}. #{value}"
     end
-    puts "#{say}"
+    puts "#{say_this}"
   end
 end
